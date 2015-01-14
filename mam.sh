6b73c095cf3b26e25309e8b301230876421701a7
lehjs <lehjs@LenovoPC.(none)>
2015-01-14 21:06:45 +0200

    add some functions to script

--- mam.sh
+++ mam.sh
@@ -4,50 +4,74 @@
 # help make your machine ready
 # for fun
 
-# decare variables
+# declare variables here 
 script_name=$1
 script_version="0.1"
 dev_path="/usr/bin/"
-# end of var block
-# func block
-# 
-check_packname() {
+
+#download package
+download() {
+	
+	local dname=$1
+	echo -e "\033[1;33m installing $dname... \033[1;30m"
+	sudo apt-get install $dname
+}
+# check is package already exist
+check_packgn() {
 	name=$1
 	if [ "$name" != "" ]; then
 		if [ -e "$dev_path$name" ]; then
-			echo "($name) :: already installed"
+			echo -e "\033[1;37m ($name) :: already installed"
 		else
-			echo "($name) :: not installed. begging downloading"
+			echo -e "\033[1;32m ($name) :: not installed."
+			download $name
 		fi
 	else
 		echo -e "\033[0;31menter package name p_install() func"
 	fi
 }
-
+helpful() {
+	if [ -e "$HOME/mam_log.txt" ]; then
+		echo "you have good setup for basic"
+	else
+		echo "nice to help you ^^ txh for usage"
+	fi 
+}
+#make pre updates
+pre_upgd() {
+	echo -e "- begging prefixing packages..."
+	# sudo apt-get autoremove;
+	# sudo apt-get autoclean; 
+	# sudo apt-get upgrade; 
+	# sudo apt-get update
+	echo -e "\033[1;32m- ready to installing..."
+}
 p_install() {
-	check_packname $1
+	check_packgn $1
 }
 basic_setup() {
-	echo -e "\033[1;30m running basic setup...."
+	echo -e "\033[1;30m- running basic setup..."
+	pre_upgd
 	p_install ruby
 	p_install git
 	p_install php
-	p_install gg
+	p_install vim
 }
 # end of dunc block
 #script body
 echo -e "\033[1;30m(v$script_version)\033[0;35m hello, $USER! thx for using  $script_name ..."
 echo -e "\033[1;36mlets make some magic with your laptop ."
-echo -e "\033[1;36mwhat type of setup do you want? \033[1;33m[basic/pro]: \033[1;30m"
+echo -e "\033[1;36mwhat type of setup do you want? \033[1;33m[basic/pro]: \033[1;36m"
 
-read -p " -> " setup_type;
+read -p "   -> " setup_type;
 
 if  [ "$setup_type" != "" ]; then
 		if [ "$setup_type" == "basic" ]; then
-			echo -e "\033[1;32mok, you want BASIC development setup. Cool ..."
+			echo -e "\033[1;32mok, you want <basic> development setup. Cool ..."
 			basic_setup
+			# helpful
 		elif [ "$setup_type" == "pro" ]; then
-			echo -e "\033[1;32mok, you want PRO development setup. Nice ..."
+			echo -e "\033[1;32mok, you want <pro> development setup. Nice ..."
 		else
 			echo -e "\033[0;30m (error) \033[0;31muncorrect setup type. Close. "
 			exit 1
@@ -55,10 +79,5 @@ if  [ "$setup_type" != "" ]; then
 	else
 	echo -e "\033[0;30m (error) \033[0;31muncorrect setup type. Close."
 	exit 1
-fi
-
-# end of script
-
-exit 0
-
+	fi
 
